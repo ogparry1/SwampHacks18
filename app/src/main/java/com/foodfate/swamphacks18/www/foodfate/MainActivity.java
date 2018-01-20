036918fbc95674ce99902e994a46cc06faf00df4
John Parry <ogparry1@ufl.edu>
2018-01-20 01:07:54 -0500

    first commit

---
+++ app/src/main/java/com/foodfate/swamphacks18/www/foodfate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foodfate.swamphacks18.www.foodfate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
